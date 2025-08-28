a5259feb020a4916fc65a8359b95d38b7ccc471e
Sumit Prajapat <sumitprajapat8684@gmail.com>
2025-08-28 19:00:13 +0530

    asdfa

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ lock "~> 3.19.2"
 set :application, 'urlshortner'
 set :repo_url, 'git@github.com:prajapatsumit1015/aws_test.git'
 set :branch, :master
-set :deploy_to, '/home/deploy/urlshortner'
+set :deploy_to, '/home/ubuntu/urlshortner'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
